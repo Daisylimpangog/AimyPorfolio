082b163f79d4c54c3a91cc05f4b4a530cd5b87eb
Daisylimpangog <119760453+Daisylimpangog@users.noreply.github.com>
2023-12-14 08:12:40 +0800

    push

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
   $(window).scroll(function () {
-    // sticky navbar on scroll script
+    // Sticky navbar on scroll script
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
 
-    // scroll-up button show/hide script
+    // Scroll-up button show/hide script
     if (this.scrollY > 500) {
       $(".scroll-up-btn").addClass("show");
     } else {
@@ -15,15 +15,15 @@ $(document).ready(function () {
     }
   });
 
-  // slide-up script
+  // Slide-up script
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
-    // removing smooth scroll on slide-up button click
+    // Removing smooth scroll on slide-up button click
     $("html").css("scrollBehavior", "auto");
   });
 
   $(".navbar .menu li a").click(function () {
-    // applying again smooth scroll on menu items click
+    // Applying smooth scroll again on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
@@ -46,12 +46,12 @@ $(document).ready(function () {
     loop: true,
   });
 
-  // owl carousel script
+  // Owl Carousel script
   $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
-    autoplayTimeOut: 2000,
+    autoplayTimeout: 2000, // Note the corrected property name
     autoplayHoverPause: true,
     responsive: {
       0: {
